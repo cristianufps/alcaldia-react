eee8ca0f72ad8c91adb83d4c12c51857a090b001
NelsonAmayaCalderon19 <nelsoneduardoac@ufps.edu.co>
2021-06-16 18:45:17 -0500

    Editando Formulario de Registro de Eventos

--- src/components/Admin/NewEvento.js
+++ src/components/Admin/NewEvento.js
@@ -1,10 +1,11 @@
 
 import React from 'react';
 import { FcNews }  from "react-icons/fc";
-import InputLabel from '@material-ui/core/InputLabel';
+/*import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import {Row, Col, Form, Input, Label, FormGroup, Button} from 'reactstrap';
+import MenuItem from '@material-ui/core/MenuItem';*/
+import {Row, Col, Form, Input, Label, FormGroup, Button, Select} from 'reactstrap';
 import '../styles/NewEvento.css';
 
 class NewEvento extends React.Component{
@@ -42,15 +43,14 @@ class NewEvento extends React.Component{
                 <Input type="text" id="link" name="link"></Input>
                 </FormGroup>
                 <FormGroup className="form_group">
-                <InputLabel id="label"><strong>Categoria</strong></InputLabel>
-<Select labelId="label" id="select" value="1">
-<MenuItem value="1">Seleccione una Categoria</MenuItem>
-  <MenuItem value="EDUCACION">Educación</MenuItem>
-  <MenuItem value="MODA">Moda</MenuItem>
-  <MenuItem value="SEGURIDAD VIAL">Seguridad Vial"</MenuItem>
-  <MenuItem value="GASTRONIMIA">Gastronomía</MenuItem>
-</Select>
-                
+                <Label id="label" className="category"><strong>Categoria: </strong></Label>
+<select  id="select">
+  <option value="EDUCACION">Educación</option>
+  <option value="MODA">Moda</option>
+  <option value="SEGURIDAD VIAL">Seguridad Vial</option>
+  <option value="GASTRONIMIA">Gastronomía</option>
+</select>
+
                 </FormGroup>
                 <FormGroup className="form_group">
                 <Button color="success">Agregar</Button>
